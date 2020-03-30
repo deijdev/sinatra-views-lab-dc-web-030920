cbe06743c5026bc8a053c5ad4851479e5f7f4491
Deijah Wilson <deijahwilson1@gmail.com>
2020-03-30 13:31:39 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,32 @@
 class App < Sinatra::Base
 
+	resp = Rack::Response.new
+	# req = Rack::Request.new
+
 	get '/' do
 		erb :index
 	end
 
+	get '/hello' do
+		resp.status = 200 
+		erb :hello
+	end
 
+	get '/goodbye' do
+		resp.status = 200 
+		@name = "Joe"
+		erb :goodbye
+	
+
+	end
+
+	get '/date' do 
+		resp.status = 200 
+		@date = Date.today.strftime("%A, %B %d, %Y")
+		erb :date  
+		# else
+		# resp.status = 400
+		# "Your application is not responding to GET /date. Did you create that route?"
+	end
 end
+
